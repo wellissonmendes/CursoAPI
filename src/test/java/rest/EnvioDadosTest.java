31238966f9b22c7614f410e01f4a57a160a9d463
wellissonmendes <wellisson.mendes@gmail.com>
2023-10-24 07:28:28 -0300

    Aula 38 Header

--- src/test/java/rest/EnvioDadosTest.java
+++ src/test/java/rest/EnvioDadosTest.java
@@ -41,4 +41,20 @@ public class EnvioDadosTest {
                 .contentType(containsString("utf-8"))
         ;
     }
+
+
+    @Test
+    public void deveEnviarValorViaHeader() {
+
+        given()
+                .log().all()
+                .accept(ContentType.XML)
+                .when()
+                .get("https://restapi.wcaquino.me/v2/users")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.XML)
+        ;
+    }
 }
