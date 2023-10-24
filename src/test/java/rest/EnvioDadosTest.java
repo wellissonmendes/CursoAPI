266615b470d1b9fa8db624a6ca3ea47971f40ec6
wellissonmendes <wellisson.mendes@gmail.com>
2023-10-24 07:19:05 -0300

    Aula 36 query

---
+++ src/test/java/rest/EnvioDadosTest.java
@@ -0,0 +1,24 @@
+package rest;
+
+import com.sun.javafx.sg.prism.NGImageView;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class EnvioDadosTest {
+
+    @Test
+    public void deveEnviarValorViaQuery(){
+
+        given()
+                .log().all()
+                .when()
+                .get("https://restapi.wcaquino.me/v2/users?format=json")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                ;
+    }
+}
