98744449847730e287fe402dc6475d6d271c8335
wellissonmendes <wellisson.mendes@gmail.com>
2023-10-26 07:48:54 -0300

    Aula 45 validando Json

--- src/test/java/rest/SchemaTest.java
+++ src/test/java/rest/SchemaTest.java
@@ -2,6 +2,7 @@ package rest;
 
 import io.restassured.RestAssured;
 import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.Test;
 import org.xml.sax.SAXParseException;
 
@@ -36,4 +37,21 @@ public class SchemaTest {
 
         ;
     }
+
+
+
+    @Test
+    public void deveValidarSchemaJson() {
+        given()
+                .log().all()
+                .when()
+                .get("http://restapi.wcaquino.me/users")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("users.json"))
+
+        ;
+    }
+
 }
